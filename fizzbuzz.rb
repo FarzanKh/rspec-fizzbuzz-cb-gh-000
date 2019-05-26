28408655fd32b734ed85a1aeb9daf22a05f8683e
Farzan Khoobchehr <farzan_khoobchehr@yahoo.com>
2019-05-26 02:06:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(value)
-  if value%3 == 0
-    "Fizz"
-  elseif value%5 == 0
-    "Buzz"
+def fizzbuzz(int)
+  if int%3 == 0
+    puts "Fizz"
+  elseif int%5 == 0
+    puts "Buzz"
   
-  elseif value%3 == 0 && value%5 ==0
-    "FizzBuzz"
+  elseif int%3 == 0 && value%5 ==0
+    puts "FizzBuzz"
   end
 end
